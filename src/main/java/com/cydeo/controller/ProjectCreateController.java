90d47281e76f3055d407ea947644f9319d5021d7
Yuliia Popelniak <popelniak96@gmail.com>
2024-04-08 21:57:04 -0500

    Project Controller

---
+++ src/main/java/com/cydeo/controller/ProjectCreateController.java
@@ -0,0 +1,22 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.UserDto;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/project")
+public class ProjectCreateController {
+
+        @GetMapping("/create")
+        public String createUser(Model model){
+
+
+
+            return "project/create";
+
+        }
+}
